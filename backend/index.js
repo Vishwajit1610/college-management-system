246cd3746f1fab453301d40dda2a8c2cf4f0e2ff
Vishwajit Mohol <moholvishwajeet@gmail.com>
2026-02-01 15:18:53 +0530

    feat: connect MongoDB and add Student model

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,41 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+
 const express = require("express");
 const app = express();
 
 app.use(express.json());
 
-let students = [];
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB Connected."))
+.catch(err => console.error(err));
+
 
 app.get("/", (req, res) => {
     res.send("Backend Working");
 });
 
-app.post("/students", (req, res) => {
-    students.push(req.body);
-    res.json(students);
-});
+const Student = require("./models/Student");
 
-app.get("/students", (req, res) => {
-    res.json(students);
+app.post("/students", async (req, res) => {
+    try {
+        const student = await Student.create(req.body);
+        res.json(student);
+    }
+    catch (err){
+        res.status(500).json({error: err.message}); 
+    }
+})
+
+app.get("/students", async (req, res) => {
+    try {
+        const students = await Student.find();
+        res.json(students);
+}   catch(err) {
+        res.status(500).json({error: err.message});
+    }
 });
 
 app.listen(3000, () => {
-    console.log("Server running on port 3000.");
+    console.log("Server running on port 3000");
 });
